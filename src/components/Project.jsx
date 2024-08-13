a5af76354919dfb74a3e12c4357478a54541b223
Connor <connorlwhite@icloud.com>
2024-08-13 13:39:52 -0400

    Decoupled Projects item

---
+++ src/components/Project.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Project.css';
+import random from '../assets/random.webp';
+
+const Project = (props) => {
+    return (
+        <>
+            <div>
+                <h1 className="project-name">{props.name}</h1>
+                <img className="project-img" src={props.image} />
+                <p className="project-description">{props.descrip}</p>
+                <p className="instru">Click on image to view the Github Repository.</p>
+            </div>
+        </>
+    );
+}
+
+export default Project;
