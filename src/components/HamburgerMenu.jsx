8998eda059db0c4a3a5b9ef88695350e4ed59db1
Connor <connorlwhite@icloud.com>
2024-08-14 00:16:28 -0400

    Fixed import bug with hamburger menu

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import '../Styles/HamburgerMenu.css';
+import '../styles/HamburgerMenu.css';
 import {Link} from 'react-router-dom';
 
 const HamburgerMenu = () => {
