b38353d2b607a54f5e82e2dfd7babb894ac8a910
Connor <connorlwhite@icloud.com>
2024-08-13 22:16:30 -0400

    Finished projects completely. will be working on wireframes next

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,10 +6,15 @@ const Project = (props) => {
     return (
         <>
             <div>
+               
                 <h1 className="project-name">{props.name}</h1>
-                <img className="project-img" src={props.image} />
+                <a href={props.link}>
+                    <img className="project-img" src={props.image} />  
+                </a>
+
                 <p className="project-description">{props.descrip}</p>
                 <p className="instru">Click on image to view the Github Repository.</p>
+                
             </div>
         </>
     );
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,18 +2,22 @@ import React, {useState} from 'react';
 import '../styles/Projects.css';
 import random from '../assets/random.webp';
 import Project from '../components/Project'
+import Sudoku from '../assets/sudoku.png';
+import bmsImg from '../assets/bms.png';
 
 const Projects = () => {
     const [sudoku, setSudoku] = useState({
         name: "TheRandomSudokuGenerator",
-        image: random,
+        link: "https://github.com/ConnorWhite9/Soduku",
+        image: Sudoku,
         descrip: "This project was built in Flask and incorporated creating a backpropagation algorithm in Python to generate the sudokus and using Javascript to create an easy to use user interface.",
     })
 
 
     const [bms, setBms] = useState({
         name: "Budgeting Made Simple",
-        image: random,
+        link: "https://github.com/danvluewubley/hackathon-budget-app",
+        image: bmsImg,
         descrip: "This project was built with Flask Blueprints. I personally worked on the backend using fetch API calls to send message to the server where I then used SQLAlchemy to manipulate information within our SQLITE database."
 
     })
@@ -21,20 +25,12 @@ const Projects = () => {
         <>
             <div class="container">
                 <div class="left-column">
-                    {/* <h1 className="project-name">TheRandomSudokuGenerator</h1>
-                    <img className="project-img" src={random} />
-                    <p className="project-description">This project was built in Flask and incorporated creating a backpropagation algorithm in Python to generate the sudokus and using Javascript to create an easy to use user interface.</p>
-                    <p className="instru">Click on image to view the Github Repository.</p> */}
-
-                    <Project name={sudoku.name} image={sudoku.image}  descrip={sudoku.descrip} />
+                    
+                    <Project name={sudoku.name} image={sudoku.image} link={sudoku.link}  descrip={sudoku.descrip} />
                 </div>
                 <div class="right-column">
-                    {/* <h1 className="project-name">Budgeting Made Simple</h1>
-                    <img className="project-img" src={random} />
-                    <p className="project-description"></p>
-                    <p className="instru">Click on image to view the Github Repository.</p> */}
-
-                    <Project name={bms.name} image={bms.image} descrip={bms.descrip} />
+                    
+                    <Project name={bms.name} image={bms.image} link={bms.link} descrip={bms.descrip} />
                 </div>
             </div>
         </>
